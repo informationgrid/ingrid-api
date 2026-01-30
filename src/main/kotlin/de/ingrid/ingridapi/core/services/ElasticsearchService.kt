7b90f66c381f61a0b5e816f85277d8d31bba7483
André Wallat <andre.wallat@wemove.com>
2026-01-30 12:40:32 +0100

    Reduce Elasticsearch error logging (#internal-5458)

--- src/main/kotlin/de/ingrid/ingridapi/core/services/ElasticsearchService.kt
+++ src/main/kotlin/de/ingrid/ingridapi/core/services/ElasticsearchService.kt
@@ -17,6 +17,10 @@ import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
 
+class SearchException(
+    message: String,
+) : Exception(message)
+
 class ElasticsearchService(
     config: AppConfig,
 ) {
@@ -39,14 +43,31 @@ class ElasticsearchService(
     suspend fun search(rawQuery: String): SearchResult {
         val indices =
             getActiveIndices().joinToString(",").also { log.debug { "Searching in indices: $it" } }
-        val response = client.search(indices, rawJson = rawQuery, ignoreUnavailable = true)
+        try {
+            val response = client.search(indices, rawJson = rawQuery, ignoreUnavailable = true)
 
-        log.debug { "Found ${response.hits?.hits?.size} hits on indices: $indices" }
-        return SearchResult(
-            response.total,
-            Json.encodeToJsonElement(response.hits?.hits ?: emptyList()) as JsonArray,
-            response.aggregations,
-        )
+            log.debug { "Found ${response.hits?.hits?.size} hits on indices: $indices" }
+            return SearchResult(
+                response.total,
+                Json.encodeToJsonElement(response.hits?.hits ?: emptyList()) as JsonArray,
+                response.aggregations,
+            )
+        } catch (ex: Exception) {
+            val message = ex.message
+            val reasonString = "\"reason\":"
+            val reason =
+                if (message != null && message.contains(reasonString)) {
+                    message
+                        .substringAfter(reasonString)
+                        .substringBefore(",")
+                        .trim('"', ' ')
+                } else {
+                    message
+                }
+            log.error { "Error while searching in indices: $reason" }
+            log.debug(ex) { "Full error details" }
+            throw SearchException("Error while searching in indices: $reason")
+        }
     }
 
     // TODO: add caching to this function
@@ -56,8 +77,7 @@ class ElasticsearchService(
                 query = term("active", "true")
                 from = 0
                 resultSize = 100
-            }
-            .parseHits<JsonObject>()
+            }.parseHits<JsonObject>()
             .mapNotNull { it["linkedIndex"]?.jsonPrimitive?.content }
 }
 

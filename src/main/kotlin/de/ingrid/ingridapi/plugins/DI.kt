849642cf791aadbc814fef88b40ba97f725cf77d
André Wallat <andre.wallat@wemove.com>
2026-02-25 15:04:24 +0100

    Fix DI

--- src/main/kotlin/de/ingrid/ingridapi/plugins/DI.kt
+++ src/main/kotlin/de/ingrid/ingridapi/plugins/DI.kt
@@ -5,18 +5,14 @@ import de.ingrid.ingridapi.core.services.ElasticsearchService
 import de.ingrid.ingridapi.ogc.records.services.RecordsService
 import de.ingrid.ingridapi.portal.services.CatalogService
 import io.ktor.server.application.Application
-import io.ktor.server.application.install
-import io.ktor.server.plugins.di.DI
 import io.ktor.server.plugins.di.dependencies
 import io.ktor.server.plugins.di.provide
 
 fun Application.configureDi() {
-    install(DI) {
-        dependencies {
-            provide(::AppConfig)
-            provide(::CatalogService)
-            provide(::ElasticsearchService)
-            provide(::RecordsService)
-        }
+    dependencies {
+        provide(::AppConfig)
+        provide(::CatalogService)
+        provide(::ElasticsearchService)
+        provide(::RecordsService)
     }
 }
